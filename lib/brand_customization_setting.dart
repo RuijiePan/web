3858be909ce91370d70f1b8ff5ccf9fec24cb165
panruijie <panruijie@yy.com>
2021-03-03 21:35:26 +0800

    flutter代码

--- lib/brand_customization_setting.dart
+++ lib/brand_customization_setting.dart
@@ -1,68 +1,22 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
+/**
+ * Created on 2/22/21
+ * @author panruijie
+ * 设置品牌水印定制
+ */
+class BrandCustomizationSetting extends StatelessWidget {
   static const String _PREVIEW_URL =
       "http://hk-facex-akm.biugoing.com/biugolite/admin/file/1614049891leWAYHQg.png?w=888&h=887";
   static const String _PC_URL = "https://marki-user.biusq.com";
   static const String _WECHAT_NUMBER = "16626414008";
 
+  BrandCustomizationSetting();
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Stack(
       children: <Widget>[
         //白色背景
@@ -108,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
             backgroundColor: Colors.transparent,
             elevation: 0,
           ),
-          body: _body(context),
+          body: Container(),
         )
       ],
     );
@@ -175,27 +129,7 @@ class _MyHomePageState extends State<MyHomePage> {
     } on Exception catch (error) {
       print("error = ${error.toString()}");
     }
-    return SingleChildScrollView(
-        child: CachedNetworkImage(
-            imageUrl: _PREVIEW_URL,
-            placeholder: (context, url) => AspectRatio(
-                aspectRatio: width * 1.0 / height,
-                child: Container(
-                    width: double.infinity,
-                    height: double.infinity,
-                    alignment: Alignment.center)),
-            imageBuilder: (context, imageProvider) => AspectRatio(
-              aspectRatio: width * 1.0 / height,
-              child: Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: imageProvider,
-                      fit: BoxFit.scaleDown,
-                    ),
-                  )),
-            )));
+    return Container();
   }
 
   //图片底部文字
@@ -235,9 +169,9 @@ class _MyHomePageState extends State<MyHomePage> {
       children: <Widget>[
         Expanded(
             child: Divider(
-              height: 1,
-              color: Color(0xffE9ECF3),
-            )),
+            height: 1,
+            color: Color(0xffE9ECF3),
+          )),
         SizedBox(width: 10),
         Text(
           text,
@@ -277,14 +211,10 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Container(
                   color: Color(0xFF2E5BFF),
                   padding: EdgeInsets.all(12),
-                  child: Text("复制",
+                  child: Text("copy",
                       style: TextStyle(color: Colors.white, fontSize: (16))),
                 ),
-                onTap: () {
-                  Clipboard.setData(ClipboardData(text: _PC_URL)).then((value) {
-                    //YFlutterToast.showToast(S.of(context).copied_open_in_pc);
-                  });
-                })
+                onTap: () {})
           ],
         ),
       ),
@@ -344,7 +274,7 @@ class _MyHomePageState extends State<MyHomePage> {
         Expanded(
             child: GestureDetector(
               child: Container(
-                  child: Text("复制",
+                  child: Text("copy",
                       style: TextStyle(
                           color: Color(0xFF2E5BFF),
                           fontSize: (14),
